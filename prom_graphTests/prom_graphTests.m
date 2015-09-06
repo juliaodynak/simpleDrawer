d231ed632687fc4706405063f5cfcd31d8879119
juliaodynak <julia.odynak@gmail.com>
2015-09-06 16:24:31 +0300

    Initial Commit

---
+++ prom_graphTests/prom_graphTests.m
@@ -0,0 +1,40 @@
+//
+//  prom_graphTests.m
+//  prom_graphTests
+//
+//  Created by adminaccount on 9/6/15.
+//  Copyright (c) 2015 pelekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface prom_graphTests : XCTestCase
+
+@end
+
+@implementation prom_graphTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
