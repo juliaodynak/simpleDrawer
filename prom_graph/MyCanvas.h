6f45ab75525b68c46bc6f9fa7e3bc00359f1ef8b
juliaodynak <julia.odynak@gmail.com>
2015-09-07 12:55:51 +0300

    new project

---
+++ prom_graph/MyCanvas.h
@@ -0,0 +1,15 @@
+//
+//  MyCanvas.h
+//  prom_graph
+//
+//  Created by adminaccount on 9/6/15.
+//  Copyright (c) 2015 pelekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCanvas : UIView
+@property BOOL touched;
+
+
+@end
---
+++ prom_graph/MyCanvas.m
@@ -0,0 +1,25 @@
+//
+//  MyCanvas.m
+//  prom_graph
+//
+//  Created by adminaccount on 9/6/15.
+//  Copyright (c) 2015 pelekh. All rights reserved.
+//
+
+#import "MyCanvas.h"
+
+@implementation MyCanvas
+
+
+
+
+- (void)drawRect:(CGRect)rect {
+    
+
+}
+
+
+
+
+
+@end
