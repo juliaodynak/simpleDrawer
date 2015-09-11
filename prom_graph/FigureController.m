4ac316c2361bd588e13009d6416e7fa27bacdde8
juliaodynak <julia.odynak@gmail.com>
2015-09-11 15:17:52 +0300

    Added function CGrectIntersectsRect.

--- prom_graph/FigureController.m
+++ prom_graph/FigureController.m
@@ -9,9 +9,13 @@
 #import "FigureController.h"
 #import "MyCanvas.h"
 
+static NSInteger const kNumberOfFigures = 10;
+
 @interface FigureController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *display;
+@property (nonatomic, strong)  NSMutableArray *figures;
+@property (nonatomic, assign) NSInteger counter;
 
 @end
 
@@ -23,11 +27,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    for (int item = 0; item < 20; ++item)
+    [self createFigures];
+    _figures = [[NSMutableArray alloc] init];
+}
+
+- (void)createFigures
+{
+    _figures = [[NSMutableArray alloc] init];
+    _counter = 0;
+    while (_counter != kNumberOfFigures)
     {
         [self placeFigure];
+        
     }
+    
 }
 
 - (void)placeFigure
@@ -36,30 +49,45 @@
     NSInteger colorStroke = ((float)rand() / (float)RAND_MAX) * MCColorChoiseCount;
     NSInteger colorFill = ((float)rand() / (float)RAND_MAX) * MCColorChoiseCount;
     MyCanvas *ob;
-    if (type!=4 && type!=9)
-    {
-        ob = [[MyCanvas alloc] initWithType: type:colorStroke: colorFill];
-    }
-    if (type == 4)
-    {
-        ob = [[MyCanvas alloc] initWithType: MCFigureTypeNAngles: colorStroke: colorFill: 6];
-    }
-    if (type == 9)
-    {
-        ob = [[MyCanvas alloc] initWithType:MCFigureTypeNAngles:colorStroke: colorFill: 12];
-    }
-    //ob.frame = CGRectMake(0, 100, 375, 375);
-    CGSize size = self.view.frame.size;
-    CGFloat figureSize = 100 + ((float)rand() / (float)RAND_MAX);
+   
     
-    CGRect figureFrame = CGRectMake(((float)rand() / (float)RAND_MAX) * (size.width - figureSize),
-                                    ((float)rand() / (float)RAND_MAX) * (size.height - figureSize),
-                                    figureSize, figureSize);
-    
-    ob.frame = figureFrame;
-    //    ob = [[MyCanvas alloc] initWithType:MCFigureTypeCircle];
-    //    ob.frame = CGRectMake(0, 100, 375, 375);
-    [self.view addSubview:ob];
+        if (type!=4 && type!=9)
+        {
+            ob = [[MyCanvas alloc] initWithType: type:colorStroke: colorFill];
+        }
+        if (type == 4)
+        {
+            ob = [[MyCanvas alloc] initWithType: MCFigureTypeNAngles: colorStroke: colorFill: 6];
+        }
+        if (type == 9)
+        {
+            ob = [[MyCanvas alloc] initWithType:MCFigureTypeNAngles:colorStroke: colorFill: 12];
+        }
+        CGSize size = self.view.frame.size;
+        CGFloat figureSize = 50 + ((float)rand() / (float)RAND_MAX);
+        
+        CGRect figureFrame = CGRectMake(((float)rand() / (float)RAND_MAX) * (size.width - figureSize),
+                                        ((float)rand() / (float)RAND_MAX) * (size.height - figureSize),
+                                        figureSize, figureSize);
+        
+        ob.frame = figureFrame;
+        if(_figures.count==0)
+        {
+            [self.figures insertObject:ob atIndex:0];
+        }
+        for(int i=0; i<_figures.count;i++)
+        {
+            if(CGRectIntersectsRect(figureFrame, [_figures[i] frame]))
+            {
+                break;
+            }
+            else if(i==_figures.count-1)
+            {
+                [_figures addObject:ob];
+                [self.view addSubview:ob];
+                _counter++;
+            }
+        }
 }
 
 @end
